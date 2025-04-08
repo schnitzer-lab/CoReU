aaaf17d3d9c11faa92eb4c94e5babd5616f17d3a
Vasily <OldBasilio@gmail.com>
2025-04-08 13:43:34 -0700

    removed options

--- processing_functions/unmixing/estimateFilterReg.m
+++ processing_functions/unmixing/estimateFilterReg.m
@@ -1,14 +1,11 @@
 
 %%
 
-function w = estimateFilterReg(x1, x2, wn, dn, varargin)
-% x1 = w(*)x2        
-    if(mod(wn,2) == 0) wn = wn+1; end
-    
-    options = defaultOptions();
-    if(~isempty(varargin))
-        options = getOptions(options, varargin);
-    end
+function w = estimateFilterReg(x1, x2, wn, dn, eps, noise_levels)
+      
+    if(mod(wn,2) == 0), wn = wn+1; end
+    if(nargin < 5), eps = 1; end
+    if(nargin < 6), noise_levels = []; end
 
     if(length(wn) > 1)
         W = wn;
@@ -26,26 +23,23 @@ function w = estimateFilterReg(x1, x2, wn, dn, varargin)
     u12f = mean(U1fall.*conj(U2fall),2);
     u22f = mean(U2fall.*conj(U2fall),2);
     u11f = mean(U1fall.*conj(U1fall),2);
-
-    if(isempty(options.noise_levels))
-        options.noise_levels = [...
-            sqrt(median(u11f(round(3/8*wn):round(5/8*wn)))/wn/mean(W.^2, 1)),...
-            sqrt(median(u22f(round(3/8*wn):round(5/8*wn)))/wn/mean(W.^2, 1))];
+    
+    if(isempty(noise_levels))
+        u11noise = median(u11f(round(3/8*wn):round(5/8*wn)));
+        u22noise = median(u22f(round(3/8*wn):round(5/8*wn)));
+        noise_levels = [sqrt(u11noise/wn/mean(W.^2, 1)),...
+                        sqrt(u22noise/wn/mean(W.^2, 1))];
     end
 
-    xn1 = randn(size(x1))*options.noise_levels(1);
-    xn2 = randn(size(x2))*options.noise_levels(2);
-    u12noise = mean( fft(W.*xn1(indxs), wn).*conj( fft(W.*xn2(indxs), wn)),2);
+    xn1 = randn(size(x1))*noise_levels(1);
+    xn2 = randn(size(x2))*noise_levels(2);
+    u12noise = mean( fft(W.*xn1(indxs),wn).*conj(fft(W.*xn2(indxs),wn)) ,2);
 
-    s1 = u12f./(u22f+options.eps*mean(abs(u12noise)));
-%     s1 = u12f./(u22f+options.eps*mean(abs(u22f)));
-    % s2 = u11f./(u12f+options.eps*mean(abs(u12f)));
+    s1 = u12f./(u22f+eps*mean(abs(u12noise)));
     s = s1;
+   
+    % s2 = u11f./(u12f+eps*mean(abs(u11noise)));
+    % s = (s1+s2)/2;
 
     w = real(fftshift(ifft(s)));
-end
-
-function options = defaultOptions()
-    options.noise_levels = []; % std of the gaussian noise in x1 and x2 
-    options.eps = 1; % factor to suppres everything below poisson noise level
 end
